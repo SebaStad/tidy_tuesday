1032aaf2dfb0579fb2d7be46096cde5a233945c6
Stadler <sebastian.stadler@ibp.fraunhofer.de>
2020-06-04 15:02:42 +0200

    added quali_race separation

--- 02_06_2020/02_06_2020.R
+++ 02_06_2020/02_06_2020.R
@@ -54,7 +54,7 @@ marbles %>%
   mutate(race_pos = row_number()) %>% 
   ungroup() %>% 
   select(race_pos, qual_pos) %>% 
-
+  
   lm(formula = race_pos ~ qual_pos, data = ., method = "qr") %>% 
   summary(.)  %>% 
   .$coefficients %>% 
@@ -64,12 +64,12 @@ marbles %>%
          lab = c("Intercept", "Slope")) -> lm_vars
 
 
- marbles %>% 
+marbles %>% 
   select(- source, -date, -host,- notes) %>% 
   mutate(RaceMode = stringr::str_sub(race, 3, 3)) %>%
   mutate(RaceNmbr = stringr::str_sub(race, 4, 4)) %>% 
   mutate(avg_speed = case_when(RaceMode == "Q" ~ number_laps*track_length_m/time_s,
-                                RaceMode =="R" ~ number_laps*track_length_m/time_s)) %>% 
+                               RaceMode =="R" ~ number_laps*track_length_m/time_s)) %>% 
   group_by(team_name, RaceNmbr) %>% 
   summarise(qual_pos = as.numeric(stringr::str_sub(pole[1],2,3)),
             race_points = points[2],
@@ -79,17 +79,17 @@ marbles %>%
   group_by(RaceNmbr) %>% 
   mutate(race_pos = row_number()) -> data_race_qual
 
- arrows_df <- data.frame("x" = 2,
-                         "y" = 14,
-                         "xend" = c(1.2, 1.2),
-                         "yend" = c(15.8, 14))
- 
- text_df   <- data.frame("lab" = "Starting first isnt\n always an advantage!",
-                         "x" = 2,
-                         "y" = 14,
-                         "hjust" = 1)
- 
- data_race_qual %>% 
+arrows_df <- data.frame("x" = 2,
+                        "y" = 14,
+                        "xend" = c(1.2, 1.2),
+                        "yend" = c(15.8, 14))
+
+text_df   <- data.frame("lab" = "Starting first isnt\n always an advantage!",
+                        "x" = 2,
+                        "y" = 14,
+                        "hjust" = 1)
+
+data_race_qual %>% 
   {
     ggplot(data = ., mapping = aes(x = qual_pos, y = race_pos)) +
       geom_count() +
@@ -106,7 +106,7 @@ marbles %>%
         x = "Qualification Position",
         y = "Race Position",
         caption = "TidyTuesday 02 June 2020"
-          
+        
       )+
       theme(
         panel.background = element_rect(fill = backgr_col,
@@ -118,24 +118,24 @@ marbles %>%
                                         colour = "white"),
         legend.position = "none"
       )
-     # geom_line(data= lm_cqr) 
+    # geom_line(data= lm_cqr) 
   }
 
- ggsave('Corr_Q_R_Linear.png', width = 10, height = 6)
- 
- text_df_loess   <- data.frame("lab" =c("Starting in the lower 6 positions\n will most likely result in\n no points!",
-                                        "While Starting in the upper 6 positions\n will likely result a good result!"),
-                         "x" = c(15,2),
-                         "y" = c(2,14),
-                         "hjust" = c(0,1),
-                         "vjust"= c(1,0.5))
- 
- arrows_df_loess <- data.frame("x" = c(14,4),
-                         "y" = c(4,13),
-                         "xend" = c(14, 3),
-                         "yend" = c(11, 5.5))
- 
- 
+ggsave('Corr_Q_R_Linear.png', width = 10, height = 6)
+
+text_df_loess   <- data.frame("lab" =c("Starting in the lower 6 positions\n will most likely result in\n no points!",
+                                       "While Starting in the upper 6 positions\n will likely result a good result!"),
+                              "x" = c(15,2),
+                              "y" = c(2,14),
+                              "hjust" = c(0,1),
+                              "vjust"= c(1,0.5))
+
+arrows_df_loess <- data.frame("x" = c(14,4),
+                              "y" = c(4,13),
+                              "xend" = c(14, 3),
+                              "yend" = c(11, 5.5))
+
+
 data_race_qual  %>% 
   
   {
@@ -206,11 +206,11 @@ marbles %>%
         caption = "TidyTuesday 02 June 2020"
         
       ) 
-      
+    
   }
 
 ggsave('Speeds_and_points.png', width = 10, height = 6)
- 
+
 
 marbles %>% 
   select(- source, -date, -host,- notes) %>% 
@@ -255,4 +255,47 @@ ggsave('Speeds_and_points_by_points.png', width = 10, height = 6)
 
 
 
- 
+marbles %>% 
+  select(- source, -date, -host,- notes) %>% 
+  mutate(RaceMode = stringr::str_sub(race, 3, 3),
+         RaceNmbr = stringr::str_sub(race, 4, 4),
+         avg_speed = case_when(RaceMode == "Q" ~ number_laps*track_length_m/time_s,
+                               RaceMode =="R" ~ number_laps*track_length_m/time_s)) %>%
+  group_by(team_name) %>% 
+  mutate(final_points = sum(points, na.rm=T)) %>% 
+  select(RaceMode, RaceNmbr, avg_speed, team_name, final_points) %>% 
+  {
+    ggplot(data = ., mapping = aes(x= reorder(RaceMode, avg_speed, FUN = median), 
+                                   y = avg_speed, fill = team_name)) +
+      geom_boxplot() +
+      facet_grid(~ team_name, scales = "free") +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
+      
+      #scale_fill_manual(values = rep(c(team_col),2)) +
+      # geom_text(aes(y= 0.46, x = team_name, label = final_points, color = team_name)) +
+      #  aes(fill=rep(team_col,16)) +
+      #scale_colour_manual(values = team_col) +
+      scale_fill_manual(values = team_col) +
+      #scale_fill_brewer(palette = "Set1"
+      #                  , name = "team_name") +
+      theme(
+        panel.background = element_rect(fill = backgr_col,
+                                        colour = backgr_col,
+                                        size = 0.5, linetype = "solid"),
+        panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                        colour = "white"), 
+        panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                        colour = "white"),
+        legend.position = "none"
+      ) +
+      scale_x_discrete(labels=c("Q" = "Qualy", "R" = "Race")) +
+      labs(
+        title = "Speed during Race and Qualification",
+        subtitle = "For all teams",
+        x = "",
+        y = "Speed in [m/s]",
+        caption = "TidyTuesday 02 June 2020"
+        
+      ) 
+    
+  }
